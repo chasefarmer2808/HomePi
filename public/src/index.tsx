261f681b02e4049ae0904446156feecb750888d3
chasefarmer2808 <chasefarmer94@gmail.com>
2022-11-27 19:07:10 -0500

    logo and nav

--- public/src/index.tsx
+++ public/src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import logo from "./logo.png";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <nav style={{ padding: "12px" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src={logo} />
+        <h1>PiHue</h1>
+      </div>
+    </nav>
     <App />
   </React.StrictMode>
 );
