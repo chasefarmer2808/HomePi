83c83d03fcc36c4101010961423aeac20989066c
chasefarmer2808 <chasefarmer2808@ufl.edu>
2022-12-02 17:54:31 -0500

    get state on load

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,11 +1,10 @@
-import { useRef, useState } from "react";
-import { LEDState, updateLed } from "./api/led";
+import { useEffect, useState } from "react";
+import { LEDState, updateLed, getLed } from "./api/led";
 import "./App.css";
 import { Color, ColorPicker } from "./components/ColorPicker";
 import { Switch } from "./components/Switch";
 
 function App() {
-  const ipInputRef = useRef<HTMLInputElement | null>(null);
   const [ledState, setLEDState] = useState<LEDState>({
     status: false,
     r: false,
@@ -13,26 +12,26 @@ function App() {
     b: false,
   });
 
+  useEffect(() => {
+    getLed().then(state => setLEDState(state))
+  }, [])
+
   const enableLed = async () => {
-    if (!ipInputRef.current) return;
-    await updateLed(ipInputRef.current.value, { status: true });
+    await updateLed({ status: true });
     setLEDState((c) => {
       return { ...c, status: true };
     });
   };
 
   const disableLed = async () => {
-    if (!ipInputRef.current) return;
-    await updateLed(ipInputRef.current.value, { status: false });
+    await updateLed({ status: false });
     setLEDState((c) => {
       return { ...c, status: false };
     });
   };
 
   const onColorSelect = async (color: Color) => {
-    console.log(color);
-    if (!ipInputRef.current) return;
-    await updateLed(ipInputRef.current.value, color);
+    await updateLed(color);
     setLEDState((c) => {
       return { ...c, ...color };
     });
@@ -41,11 +40,7 @@ function App() {
   return (
     <div className='App'>
       <div>
-        <h3>1. Input Raspberry Pi local IP Address and Port Number</h3>
-        <input ref={ipInputRef} placeholder='127.0.0.1:5000' />
-      </div>
-      <div>
-        <h3>2. Enable the LED if it is disabled</h3>
+        <h3>1. Enable the LED if it is disabled</h3>
         <Switch
           enabled={ledState.status || false}
           onEnable={enableLed}
@@ -53,7 +48,7 @@ function App() {
         />
       </div>
       <div>
-        <h3>3. Select a color!</h3>
+        <h3>2. Select a color!</h3>
         <ColorPicker onSelect={onColorSelect} />
       </div>
     </div>
--- public/src/api/led.ts
+++ public/src/api/led.ts
@@ -5,8 +5,13 @@ export interface LEDState {
   b?: boolean;
 }
 
-export async function updateLed(hostname: string, newState: LEDState) {
-  const resp = await fetch(`http://${hostname}/led`, {
+export async function getLed(): Promise<LEDState> {
+  const resp = await fetch(`http://${document.location.hostname}:5000/led`);
+  return resp.json();
+}
+
+export async function updateLed(newState: LEDState) {
+  const resp = await fetch(`http://${document.location.hostname}:5000/led`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(newState),
